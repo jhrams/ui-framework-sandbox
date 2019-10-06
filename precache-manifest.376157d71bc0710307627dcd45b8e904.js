0526d604cc466fa378fc1c16660a0d373ecb81a2
Jorge Ramos <jorge.ramos@thinkresearch.com>
2019-10-06 11:57:16 -0400

    Updates

---
+++ precache-manifest.376157d71bc0710307627dcd45b8e904.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "397d5837319c35a14df6580751758232",
+    "url": "/ui-framework-sandbox/index.html"
+  },
+  {
+    "revision": "2ede0f1ad0a3fb967472",
+    "url": "/ui-framework-sandbox/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3fbb31328820d0e95c01",
+    "url": "/ui-framework-sandbox/static/js/2.777eef24.chunk.js"
+  },
+  {
+    "revision": "2ede0f1ad0a3fb967472",
+    "url": "/ui-framework-sandbox/static/js/main.b10c7103.chunk.js"
+  },
+  {
+    "revision": "7054174c09b7309f59a1",
+    "url": "/ui-framework-sandbox/static/js/runtime-main.5fec0269.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/ui-framework-sandbox/static/media/logo.25bf045c.svg"
+  }
+]);
