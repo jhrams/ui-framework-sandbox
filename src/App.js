28002050d0a449a1cfa9e161e0b7bf10c25b538d
Jorge Ramos <jorge.ramos@thinkresearch.com>
2019-10-06 18:56:21 -0400

    Adding selection package to highlight particular html elements on a page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,48 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Testing from './Testing';
+import Selection from '@simonwep/selection-js';
 
 function App() {
+  Selection.create({
+      // Class for the selection-area
+      class: 'App',
+
+      // All elements in this container can be selected
+      selectables: ['.test', '.App-logo', '.test1'],
+
+      // The container is also the boundary in this case
+      boundaries: ['.App']
+  }).on('start', ({inst, selected, oe}) => {
+      // Remove class if the user isn't pressing the control key or ⌘ key
+      if (!oe.ctrlKey && !oe.metaKey) {
+
+          // Unselect all elements
+          for (const el of selected) {
+              el.classList.remove('selected');
+              inst.removeFromSelection(el);
+          }
+
+          // Clear previous selection
+          inst.clearSelection();
+      }
+
+  }).on('move', ({changed: {removed, added}}) => {
+      // Add a custom class to the elements that where selected.
+      for (const el of added) {
+          el.classList.add('selected');
+      }
+
+      // Remove the class from elements that where removed
+      // since the last selection
+      for (const el of removed) {
+          el.classList.remove('selected');
+      }
+  }).on('stop', ({inst}) => {
+      inst.keepSelection();
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,7 +58,13 @@ function App() {
         >
           Learn React
         </a>
+        <p className="test">testing 1</p>
+        <p className="test">testing 2</p>
+        <p className="test">testing 3</p>
+        <p className="test">testing 4</p>
+        <p className="test">testing 5</p>
       </header>
+      <Testing />
     </div>
   );
 }
---
+++ src/Testing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Testing() {
+  return (
+    <div className="Tesinggggg">
+      <p className="test1">testing 1</p>
+      <p className="test1">testing 2</p>
+      <p className="test1">testing 3</p>
+      <p className="test1">testing 4</p>
+      <p className="test1">testing 5</p>
+    </div>
+  );
+}
+
+export default Testing;
