8a058bf077574e01c79c2db41bb799813b7dbfb3
Jorge Ramos <jorge.ramos@thinkresearch.com>
2019-10-13 17:36:39 -0400

    Removing useless comments/spacing, adding css to make selection boundary clientHeight different from scrollHeight (this allows for the page to scroll when highlighting different elements

--- src/App.js
+++ src/App.js
@@ -6,41 +6,32 @@ import Selection from '@simonwep/selection-js';
 
 function App() {
   Selection.create({
-      // Class for the selection-area
-      class: 'selection',
-
-      // All elements in this container can be selected
-      selectables: ['.test', '.App-logo', '.test1'],
-
-      // The container is also the boundary in this case
-      boundaries: ['.App']
+    class: 'selection',
+    selectables: ['.test', '.App-logo', '.test1'],
+    boundaries: ['.App'],
   }).on('start', ({inst, selected, oe}) => {
-      // Remove class if the user isn't pressing the control key or ⌘ key
-      if (!oe.ctrlKey && !oe.metaKey) {
-
-          // Unselect all elements
-          for (const el of selected) {
-              el.classList.remove('selected');
-              inst.removeFromSelection(el);
-          }
-
-          // Clear previous selection
-          inst.clearSelection();
-      }
-
-  }).on('move', ({changed: {removed, added}}) => {
-      // Add a custom class to the elements that where selected.
-      for (const el of added) {
-          el.classList.add('selected');
-      }
-
-      // Remove the class from elements that where removed
-      // since the last selection
-      for (const el of removed) {
+    // Remove class if the user isn't pressing the control key or ⌘ key
+    if (!oe.ctrlKey && !oe.metaKey) {
+      // Unselect all elements
+      for (const el of selected) {
           el.classList.remove('selected');
+          inst.removeFromSelection(el);
       }
+      // Clear previous selection
+      inst.clearSelection();
+    }
+  }).on('move', ({changed: {removed, added}}) => {
+    // Add a custom class to the elements that where selected.
+    for (const el of added) {
+      el.classList.add('selected');
+    }
+    // Remove the class from elements that where removed
+    // since the last selection
+    for (const el of removed) {
+      el.classList.remove('selected');
+    }
   }).on('stop', ({inst}) => {
-      inst.keepSelection();
+    inst.keepSelection();
   });
 
   return (
@@ -58,23 +49,25 @@ function App() {
         >
           Learn React
         </a>
-        <p className="test">testing 1</p>
-        <p className="test">testing 2</p>
-        <p className="test">testing 3</p>
-        <p className="test">testing 4</p>
-        <p className="test">testing 5</p>
-        <p className="test">testing 2</p>
-        <p className="test">testing 3</p>
-        <p className="test">testing 4</p>
-        <p className="test">testing 5</p>
-        <p className="test">testing 2</p>
-        <p className="test">testing 3</p>
-        <p className="test">testing 4</p>
-        <p className="test">testing 5</p>
-        <p className="test">testing 2</p>
-        <p className="test">testing 3</p>
-        <p className="test">testing 4</p>
-        <p className="test">testing 5</p>
+        <div className="testing-boundary">
+          <p className="test">testing 1</p>
+          <p className="test">testing 2</p>
+          <p className="test">testing 3</p>
+          <p className="test">testing 4</p>
+          <p className="test">testing 5</p>
+          <p className="test">testing 2</p>
+          <p className="test">testing 3</p>
+          <p className="test">testing 4</p>
+          <p className="test">testing 5</p>
+          <p className="test">testing 2</p>
+          <p className="test">testing 3</p>
+          <p className="test">testing 4</p>
+          <p className="test">testing 5</p>
+          <p className="test">testing 2</p>
+          <p className="test">testing 3</p>
+          <p className="test">testing 4</p>
+          <p className="test">testing 5</p>
+        </div>
         <div className="test-div">
           <p className="testing1">testing <span className="testing2">6</span></p>
         </div>
