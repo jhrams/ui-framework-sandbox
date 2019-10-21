878b62df7b108679e8a3c47c617b4691587e2320
Jorge Ramos <jorge.ramos@thinkresearch.com>
2019-10-21 10:52:56 -0400

    moving selection variable outside of class so it doesn't create a new one everytime the component renders, installing material-ui core

--- src/App.js
+++ src/App.js
@@ -4,45 +4,61 @@ import './App.css';
 import Testing from './Testing';
 import Selection from '@simonwep/selection-js';
 
+import { Button as SButton } from 'semantic-ui-react';
+import { Button as MButton } from '@material-ui/core';
+import { Button as BButton } from 'react-bootstrap';
+
+
+const selection = Selection.create({
+  class: 'selection',
+  selectables: ['.test', '.App-logo', '.test1'],
+  boundaries: ['.App'],
+}).on('start', ({inst, selected, oe}) => {
+  // Remove class if the user isn't pressing the control key or ⌘ key
+  if (!oe.ctrlKey && !oe.metaKey) {
+    // Unselect all elements
+    for (const el of selected) {
+      el.classList.remove('selected');
+      inst.removeFromSelection(el);
+    }
+    // Clear previous selection
+    inst.clearSelection();
+  }
+}).on('move', ({changed: {removed, added}}) => {
+  // Add a custom class to the elements that where selected.
+  for (const el of added) {
+    el.classList.add('selected');
+  }
+  // Remove the class from elements that where removed
+  // since the last selection
+  for (const el of removed) {
+    el.classList.remove('selected');
+  }
+}).on('stop', ({inst}) => {
+  inst.keepSelection();
+});
+
 class App extends React.Component {
   state = {
-    selectionEnabled: true
+    selectionEnabled: true,
+    theme: 'react-semantic'
   };
 
   render() {
-    const selection = Selection.create({
-      class: 'selection',
-      selectables: ['.test', '.App-logo', '.test1'],
-      boundaries: ['.App'],
-    }).on('start', ({inst, selected, oe}) => {
-      // Remove class if the user isn't pressing the control key or ⌘ key
-      if (!oe.ctrlKey && !oe.metaKey) {
-        // Unselect all elements
-        for (const el of selected) {
-          el.classList.remove('selected');
-          inst.removeFromSelection(el);
-        }
-        // Clear previous selection
-        inst.clearSelection();
-      }
-    }).on('move', ({changed: {removed, added}}) => {
-      // Add a custom class to the elements that where selected.
-      for (const el of added) {
-        el.classList.add('selected');
-      }
-      // Remove the class from elements that where removed
-      // since the last selection
-      for (const el of removed) {
-        el.classList.remove('selected');
-      }
-    }).on('stop', ({inst}) => {
-      inst.keepSelection();
-    });
-
     this.state.selectionEnabled ? selection.enable() : selection.disable();
 
     return (
       <div className={`App ${this.state.selectionEnabled ? 'selection-enabled' : 'selection-disabled' }`}>
+        <div>
+          <button onClick={() => this.setState({ theme: 'react-semantic' })}>React-Semantic</button>
+          <button onClick={() => this.setState({ theme: 'material-ui' })}>Material-UI</button>
+          <button onClick={() => this.setState({ theme: 'react-bootstrap' })}>React-Bootstrap</button>
+        </div>
+        <div>
+          { this.state.theme === 'react-semantic' && <SButton primary>Primary</SButton> }
+          { this.state.theme === 'material-ui' && <MButton variant="contained" color="primary">Primary</MButton> }
+          { this.state.theme === 'react-bootstrap' && <BButton variant="primary">Primary</BButton> }
+        </div>
         <header className="App-header">
           <button onClick={() => {
             selection.enable();
@@ -59,17 +75,6 @@ class App extends React.Component {
             this.setState({ selectionEnabled: false });
           }}>Disable</button>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <div className="testing-boundary">
             <p className="test">testing 1</p>
             <p className="test">testing 2</p>
